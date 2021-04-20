58b9341fdcbf2ab0f9c9b82043d0d63d66074db3
Lucas <lucaspedrazoli@gmail.com>
2021-04-20 14:45:31 -0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let range = 1...10
+let isInRange1 = range ~= 5
+let isInRange2 = range ~= 110
+print("range1: \(isInRange1)")
+print("range2: \(isInRange2)")
